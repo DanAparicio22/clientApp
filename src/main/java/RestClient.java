a174e691e4c9e47d56ce27f103f5c970e766fa78
Daniel Aparicio <daparicio52@gmail.com>
2017-06-08 15:56:33 -0400

    first commit

--- src/main/java/RestClient.java
+++ src/main/java/RestClient.java
@@ -3,7 +3,9 @@ import java.io.InputStreamReader;
 
 import java.io.IOException;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 import org.apache.http.HttpResponse;
@@ -19,9 +21,14 @@ import org.apache.http.message.BasicNameValuePair;
 public class RestClient {
     public static void main(String[] args) {
         try {
-
-            while(true){
-
+            int cont=20;
+            Integer x = 960;
+            Integer y = 990;
+            while(cont!=0){
+                if(y>=1000 || x>=1000){
+                    y=500;
+                    x=300;
+                }
                 DefaultHttpClient httpClient = new DefaultHttpClient();
 
                 HttpPost postRequest = new HttpPost(
@@ -35,18 +42,22 @@ public class RestClient {
                 Integer  step = rand.nextInt(50) + 1;
                 Integer bpms = rand.nextInt(39)+52;
                 Integer distances = rand.nextInt(4)+1;
-                Float latitude = rand.nextFloat()*7+3;
-                Float longitude = rand.nextFloat()*10+5;
                 Integer calories =rand.nextInt(20)+5;
+
+
 
                 postParameters.add(new BasicNameValuePair("steps",step.toString()));
                 postParameters.add(new BasicNameValuePair("bpm", bpms.toString()));
                 postParameters.add(new BasicNameValuePair("distance", distances.toString()));
-                postParameters.add(new BasicNameValuePair("latitude", latitude.toString()));
-                postParameters.add(new BasicNameValuePair("longitude", longitude.toString()));
+                postParameters.add(new BasicNameValuePair("x", x.toString()));
+                postParameters.add(new BasicNameValuePair("y", y.toString()));
                 postParameters.add(new BasicNameValuePair("calories", calories.toString()));
                 postParameters.add(new BasicNameValuePair("user", "2"));
 
+                y+=50;
+                x+=30;
+
+                cont--;
                 postRequest.setEntity(new UrlEncodedFormEntity(postParameters));
                 postRequest.addHeader("accept", "application/json");
 
